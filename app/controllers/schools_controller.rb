5bc3e294e8a25be68181e9e86fbc0aaf677154fc
Eric Espindola <eric_e_2012@hotmail.com>
2022-05-11 01:12:13 -0700

    add conrollers, views, and routes for welcome and schools

---
+++ app/controllers/schools_controller.rb
@@ -0,0 +1,11 @@
+class SchoolsController < ApplicationController
+  def index
+    @schools = ['School 1', 'School 2', 'School 3']
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get '/', to: 'welcome#index'
+get '/schools', to: 'schools#index'
+get '/schools/new', to: 'schools#new'
+post '/schools', to: 'schools#create'
 end
