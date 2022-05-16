5eb756907e5682d978ddff11e269f99ff318e7b9
Eric Espindola <eric_e_2012@hotmail.com>
2022-05-16 01:58:20 -0700

    create method student_count to count students. Add to school show view

--- app/models/school.rb
+++ app/models/school.rb
@@ -2,4 +2,8 @@ class School < ApplicationRecord
   has_many :students
   validates_presence_of :school_name, :school_address
   validates :active, inclusion: [true, false]
+
+  def student_count
+    self.students.count
+  end
 end
