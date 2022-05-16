075b95bd60f42e2f349325ed8db2ec31cd015b6e
Eric Espindola <eric_e_2012@hotmail.com>
2022-05-16 01:56:19 -0700

    create tests for counting a school's students

--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -11,4 +11,30 @@ RSpec.describe School, type: :model do
   describe 'relationships' do
     it { should have_many :students}
   end
+
+  describe 'count relationships' do
+    it 'school can count number of students' do
+      school1 = School.create!( school_name: 'SHS',
+                                school_address: '123 abc st.',
+                                active: true)
+      school2 = School.create!( school_name: 'GHS',
+                                school_address: '456 def st.',
+                                active: true)
+      student1 = Student.create!( student_name: 'Ice Cube',
+                                  school_id: school1.id,
+                                  age: 52,
+                                  frl: true)
+      student2 = Student.create!( student_name: 'Harry Styles',
+                                  school_id: school2.id,
+                                  age: 28,
+                                  frl: false)
+      student3 = Student.create!( student_name: 'James Franco',
+                                  school_id: school1.id,
+                                  age: 44,
+                                  frl: false)
+
+      expect(school1.student_count).to eq(2)
+      expect(school2.student_count).to eq(1)
+    end
+  end
 end
