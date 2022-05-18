23ca76b68d9a8432632ae03f47e8d881c03feeec
Eric Espindola <eric_e_2012@hotmail.com>
2022-05-17 22:20:07 -0700

    create filter_frl method in School class and update index with method

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,8 @@ class Student < ApplicationRecord
   belongs_to :school
   validates_presence_of :student_name, :school_id, :age
   validates :frl, inclusion: [true, false]
+
+  def self.filter_frl
+    where("frl = true")
+  end
 end
