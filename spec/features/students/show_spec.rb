7214801cf54bad8c1e80b5cd98ea99a890a47ba2
Eric Espindola <eric_e_2012@hotmail.com>
2022-05-17 21:37:05 -0700

    add test for allowing a visitor to edit a student's info via the show page

--- spec/features/students/show_spec.rb
+++ spec/features/students/show_spec.rb
@@ -19,4 +19,25 @@ RSpec.describe 'student show page', type: :feature do
     expect(page).to have_content("updated at: #{student1.updated_at}")
 
   end
+
+  it 'as a visitor I can fill out form to edit student attributes' do
+    school1 = School.create!( school_name: 'SHS',
+                              school_address: '123 abc st.',
+                              active: true)
+    student1 = school1.students.create!( student_name: 'Ice Cube',
+                                age: 52,
+                                frl: true)
+    visit "/students/#{student1.id}"
+    click_link 'Update Student'
+    expect(current_path).to eq("/students/#{student1.id}/edit")
+
+    fill_in 'student_name', with: 'Dr. Dre'
+    fill_in 'age', with: 57
+    fill_in 'frl', with: 'false'
+    click_on "Update Student"
+
+    expect(current_path).to eq("/students/#{student1.id}")
+    expect(page).to have_content('Dr. Dre')
+    expect(page).to_not have_content('Ice Cube')
+  end
 end
